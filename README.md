d0bbd6bda5e6031fb0db46c3cda6581fde9d267b
Michael Swain <mpswain@gmail.com>
2019-02-13 21:48:19 -0500

    Initial commit of Project 1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# p1
+# Project 1
++ By: Michael Swain
++ Production URL: <http://p1.theneeners.com>
+
+## Outside resources
+* [PHP Manual](http://php.net/manual/en/index.php)
+* [Funniest Quotes From Monty Python and the Holy Grail](https://www.bostonglobe.com/arts/movies/2012/04/26/funniest-quotes-from-monty-python-and-holy-grail/xPdFqDGRqPCwd3XXY2qFnK/story.html)
+* [Google Fonts](https://fonts.google.com)
+
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>E-15 - Project 1</title>
+  <link href="css/main.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+</head>
+
+<body>
+
+  <h1>Michael Swain</h1>
+
+  <div><img src="images/mps.jpg" width="314" height="314" alt="photo of Michael Swain" title="Michael Swain"></div>
+
+  <h2>About Me</h2>
+
+  <p>Hi, I'm Michael Swain, from Buffalo. (Yes, it's snowing.)</p>
+
+  <p>I've been involved with the web since the mid-'90s (think blinking text, flaming skulls, etc.), and I took a bunch of courses in the Extension School and was enrolled in the masters in IT (software engineering) before kids and career happened.</p>
+
+  <p>I'm looking forward to a deep dive in modern web technologies and workflows. I do some work in Drupal which runs on PHP, so this course should help in that regard as well.</p>
+
+  <h2>Quotes</h2>
+
+  <?php echo '<p>Random quote goes here</p>'; ?>
+
+</body>
+
+</html>
